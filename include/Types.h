478cd93f8582b8ae15333aa76f835584556f76a6
bzgec <luclul103@gmail.com>
2019-03-17 13:51:22 +0100

    first commit

---
+++ include/Types.h
@@ -0,0 +1,33 @@
+#ifndef __Types_h
+#define __Types_h
+
+#include <stdint.h>
+
+
+// typedef     data_type     new_name     Type prefix
+typedef         uint8_t       BOOL;         // b
+typedef         uint8_t       BYTE;         // by
+typedef         uint16_t      WORD;         // w
+typedef         int16_t       SHORT;        // s
+typedef         uint32_t      DWORD;        // dw
+typedef         int32_t       LONG;         // l
+typedef         uint64_t      QWORD;        // qw
+typedef         int64_t       LWORD;        // ql
+
+/* Truth values */
+#ifndef TRUE
+#define TRUE 1
+#endif
+
+#ifndef FALSE
+#define FALSE 0
+#endif
+
+#ifndef NULL
+#define NULL 0L
+#endif
+
+#endif  // __Types_h
+
+
+
